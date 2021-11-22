07f20368e3e98478a6f634f12d5f194686a4c4ba
Your Name <email@example.com>
2021-11-22 10:57:21 -0800

    completed GET requests

---
+++ react-app/src/store/role.js
@@ -0,0 +1,75 @@
+const LOAD_ROLES = 'roles/LOAD_ROLES';
+const ADD_ROLES = "roles/ADD_ROLES"
+
+
+const getRoles = roles => ({
+    type: LOAD_ROLES,
+    roles,
+})
+
+const add_role = (roles) => ({
+    type: ADD_ROLES,
+    roles,
+  });
+
+export const loadRoles = () => async (dispatch) => {
+    const response = await fetch(`/api/roles/`);
+
+    if (response.ok) {
+        const roles = await response.json();
+        dispatch(getRoles(roles))
+    }
+}
+
+export const createRoles = (user_id, project_id, custom_name, type, quantity, description) => async (dispatch) => {
+    const response = await fetch(`/api/roles/new`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            user_id,
+            project_id,
+            custom_name,
+            type,
+            quantity,
+            description
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        dispatch(add_role(data));
+        return null;
+      } else if (response.status < 500) {
+        const data = await response.json();
+        if (data.errors) {
+          return data.errors;
+        }
+      } else {
+        return ['An error occurred. Please try again.']
+      }
+  }
+
+let initialState = { roles: null };
+
+const rolesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_ROLES:
+            const allroles = {};
+
+            for (let role of action.roles.roles) {
+                allroles[role.id] = role;
+            }
+            return allroles
+        case ADD_ROLES:
+            return {
+                ...state,
+                [action.roles.id]: action.roles,
+            }
+
+        default:
+            return state;
+    }
+};
+
+export default rolesReducer;
