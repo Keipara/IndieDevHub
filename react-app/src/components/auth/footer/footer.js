de140345e143a19fcd4f2fba244ffd67c88b2085
Your Name <email@example.com>
2021-11-28 12:07:26 -0800

    footer complete

--- react-app/src/components/auth/footer/footer.js
+++ react-app/src/components/auth/footer/footer.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import './footer.css';
 
 const Footer = () => {
 
 return (
 <div className='footer-container'>
-                <h3>Kiara Mendaros</h3>
-                <div className="Kiara-github">
-                    <a href='https://github.com/Keipara'>Github</a>
-                </div>
-                <div className="Kiara-linkedin">
-                    <a href='https://www.linkedin.com/in/kiara-mendaros-9761101b8/' className="test">LinkedIn</a>
-                </div>
-            </div>
+    <div className='kiara-mendaros'>
+        <h3>Kiara Mendaros</h3>
+    </div>
+    <div className='links-container'>
+        <div className="Kiara-github">
+            <a href='https://github.com/Keipara' className='footer-text'>Github</a>
+        </div>
+        <div className="Kiara-linkedin">
+            <a href='https://www.linkedin.com/in/kiara-mendaros-9761101b8/' className="footer-text">LinkedIn</a>
+        </div>
+    </div>
+</div>
 )
 }
 
